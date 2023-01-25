e311209c3f4e4ceddae2497ad1f1b5e1f0d7f7e0
Sean Flaherty <sflaherty36@comcast.net>
2023-01-25 15:44:03 -0500

    Rough Draft

---
+++ src/components/About/index.js
@@ -0,0 +1,77 @@
+import {
+  faNodeJs,
+  faCss3,
+  faEthereum,
+  faJsSquare,
+  faReact,
+  faPython,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              index={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious fullstack developer looking for a role as a
+            entry level software engineering in an established company with the
+            opportunity to display my skills and grow as a programmer
+          </p>
+          <p>
+            {' '}
+            My experience in previous programming based research projects,
+            Managerial positions, as well as my time on my college swimming and
+            diving team, have helped shape me into a hardworking, goal driven
+            team player. I am constantly working on ways to improve my skills
+            one problem at a time.
+          </p>
+          <p></p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faPython} color="#ff6123" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faEthereum} color="#468CFF" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faNodeJs} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
---
+++ src/components/contact/index.js
@@ -0,0 +1,96 @@
+import { useEffect, useRef, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import emailjs from '@emailjs/browser'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_uu0jwla',
+        'template_yrda7rr',
+        refForm.current,
+        'zqN12md2xfc49kHqU'
+      )
+      .then(
+        () => {
+          alert('Message sucessfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              index={15}
+            />
+          </h1>
+          <p>
+            If you have questions or requests, please dont hesitate to contact
+            me using the form below.
+          </p>
+          <p>or you can send me a direct email at sflaherty36@comcast.net</p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="message"
+                    name="message"
+                    require
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
